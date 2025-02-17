9d25d0a11ba480d9dec743bc21c3c1c5838e569f
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-02-17 12:16:27 +0000

    debug locking

--- cmake/DependencyManager.cmake
+++ cmake/DependencyManager.cmake
@@ -550,7 +550,7 @@ endfunction()
 function(DependencyManager_Declare name GIT_REPOSITORY)
     if (NOT DEFINED Declare_has_run)
         message ("first declare ${name}")
-        set(Declare_has_run ON)
+        set(Declare_has_run ON PARENT_SCOPE)
         set(lockfile ".cmake_build_lock")
         file(LOCK "${lockfile}" GUARD FILE TIMEOUT 1000) # don't allow multiple instances of cmake because of population of dependencies
     endif()
